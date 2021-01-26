838bd49df4a2ec9286ace956cc2f40f144926775
Moslm Iraqi <mike7olwo@gmail.com>
2021-01-27 00:09:30 +0300

    hmm

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
               <a className="nav-link" href="#">Link</a>
             </li> */}
             <li className="nav-item">
-              <a className="nav-link" href="#">Coming Soon... </a>
+              <a className="nav-link" href='/'>Coming Soon... </a>
             </li>
           </ul>
           <form className="form-inline mt-2 mt-md-0">
